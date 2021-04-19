18dd9b0d0ec7e0cc46f1a1807894ca4a0ab0b395
Neona646 <79280901+Neona646@users.noreply.github.com>
2021-04-19 09:29:26 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,105 @@
 
+
+const $customize = document.getElementById('customize');
+const $timer = document.getElementById('timer');
+const $date = document.getElementById('date');
+// Selecting with Jquery $(selector).action()
+
+let is24Hours = false
+
+const $twenty = document.getElementById('twenty');
+const $twelve = document.getElementById('twelve');
+
+$twenty.addEventListener('click', function(){
+    is24Hours = true;
+});
+
+$twelve.addEventListener('click' , function() {
+    is24Hours = false;
+});
+
+
+setInterval(function () {
+    const $blockA = new Date()
+const $clock = document.getElementById('clock')
+$clock.textContent = $blockA.toLocaleTimeString();
+$("#clock").css({
+    "font-size" : "8rem",
+    "color" : "white",
+    "position" : "absolute",
+    "left"  : "27%",
+    "top" : "30%"
+})
+
+    if (is24Hours === true) {
+     $clock.textContent = $blockA.toLocaleTimeString('it-IT')
+    } else if( is24Hours === false) {
+    $clock.textContent = $blockA.toLocaleTimeString('en-US')
+    }
+
+
+    const $month = document.getElementById('month')
+    $('.months').click(function(){
+        $month.textContent = $blockA.toDateString()
+    $("#month").css({
+        "color" : "white",
+        "position" : "absolute",
+        "left"  : "35%",
+        "top" : "70%",
+        "font-size" : "4rem", 
+    })
+    })
+    
+ 
+}, 1000)
+
+
+
+
+
+
+
+
+
+
+
+
+/// Background
+fetch('https://api.nasa.gov/planetary/apod?api_key=OnKN6Z9rYZXr6JwsAW2jGaxyXMG5VZRKVBQOMuYa')
+.then(function(response) {
+    return response.json()
+}) .then(function(json) {
+    console.log(json.url)
+})
+
+
+
+// Local Storage
+
+localStorage.setItem('name', 'date', fetch())
+localStorage.getItem('name', 'date', fetch())
+
+
+
+
+
+// Weeks and month 
+
+
+/*$month.textContent = $blockA.toDateString()
+$("#month").css({
+    "color" : "white",
+    "position" : "absolute",
+    "left"  : "35%",
+    "top" : "70%",
+    "font-size" : "4rem", 
+}) */
+
+
+//Set Interval 
+
+
+
 const $customize = document.getElementById('customize');
 const $timer = document.getElementById('timer');
 const $date = document.getElementById('date');
@@ -96,4 +197,5 @@ $("#month").css({
 
 
 //Set Interval 
+
 
